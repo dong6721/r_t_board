b2ee6b0fb33329e6fbc01ace525520f7ca57279c
dong6721 <dong6721@gmail.com>
2020-12-28 17:19:32 +0900

    comment typing update

--- socket/socket.js
+++ socket/socket.js
@@ -27,62 +27,67 @@ module.exports = (server) => {
     socket.on('error', (error) => {
       console.error(error);
     });
-    socket.on('reply', (data) => {
-      console.log(data);
-    });
+
     //write new comment
     socket.on("*",(event, data) => {
       var board = db("board", "postSchema");
       console.log(event);
       console.log(data);
       var evt = event.split('/');
+      //check event
       if(evt[0] === "cmt"){
-        try {
-          //save to DB & send broadcast
-          var newDate = new Date();
-          var time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS');
-          var cmt_num;
-          //save to DB
-          board.findOne({
-            index: evt[2]
-          }, (err, res) => {
-            console.log("test");
-            if (err) {
-              console.log("find one error!", err);
-              return res.status(500).json({
-                error: err
-              });
-            }
-            res.comment.push({
-              nickname: data.id,
-              comment: data.text,
-              date: time
-            });
-            res.save((err,res) => {
-              console.log("test3");
+        if(evt[3] === "typing"){
+          // 'cmt/:boardname/:postindex/typing'
+          socket.broadcast.emit(event,data);
+          socket.emit(event,data);
+        }
+        else {
+          // 'cmt/:boardname/:postindex'
+          try {
+            //save to DB & send broadcast
+            var newDate = new Date();
+            var time = newDate.toFormat('YYYY-MM-DD HH24:MI:SS');
+            var cmt_num;
+            //save to DB
+            board.findOne({
+              index: evt[2]
+            }, (err, res) => {
               if (err) {
-                console.log(err);
-              } else {
-                //save success
-                console.log("saved!");
-                cmt_num = res.comment.length - 1;
-                //send broadcast
-                var broadcast_data = {
-                  cmt_id:cmt_num,
-                  nickname: data.id,
-                  comment: data.text,
-                  date: time
-                };
-                socket.broadcast.emit(event,broadcast_data);
-                socket.emit(event,broadcast_data);
+                console.log("find one error!", err);
+                return res.status(500).json({
+                  error: err
+                });
               }
+              res.comment.push({
+                nickname: data.id,
+                comment: data.text,
+                date: time
+              });
+              res.save((err,res) => {
+                if (err) {
+                  console.log(err);
+                } else {
+                  //save success
+                  console.log("saved!");
+                  cmt_num = res.comment.length - 1;
+                  //send broadcast
+                  var broadcast_data = {
+                    cmt_id:cmt_num,
+                    nickname: data.id,
+                    comment: data.text,
+                    date: time
+                  };
+                  socket.broadcast.emit(event,broadcast_data);
+                  socket.emit(event,broadcast_data);
+                }
+              });
             });
-          });
-        } catch (e) {
-          //error
-          console.log("error : ", e);
-          socket.broadcast.emit(event,{id:data.id,err:"write_err"});
-          socket.emit("error","cmt err!");
+          } catch (e) {
+            //error
+            console.log("error : ", e);
+            socket.broadcast.emit(event,{id:data.id,err:"write_err"});
+            socket.emit("error","cmt err!");
+          }
         }
       }
     });
